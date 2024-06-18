d284fb60d4e052517aa7a59f9e3107389cd04008
cristian3228624186 <cris333tian333@gmail.com>
2024-06-18 11:06:25 -0500

    18/07/2024

---
+++ colaborativos01.dart
@@ -0,0 +1,18 @@
+void main(){
+  print("Colaborativo 1");
+
+  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // Mostrar la lista
+  for (int i = 0; i < numeros.length; i++) {
+    print("El vector es: ");
+    print(numeros[i]);
+  }
+
+  // Código para buscar un elemento en la lista
+  int numBuscar = 3;
+  for (int i = 0; i <numeros.length; i++) {
+    if (numBuscar == numeros[i]) {
+      print("Se encontró el número en la posición $i");
+    }
+  }
+}
